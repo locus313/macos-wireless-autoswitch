bfbed3d41891033522dc8582fb0f8c777ecf9a1e
Patrick Lewis <Patrick.Lewis@modusbox.com>
2022-03-19 00:05:32 -0700

    change to use cp instead of mv

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 
 sudo mkdir -p /Library/Scripts/NetBasics/
 
-sudo mv wireless.sh /Library/Scripts/NetBasics/
+sudo cp wireless.sh /Library/Scripts/NetBasics/
 
 sudo chmod 755 /Library/Scripts/NetBasics/wireless.sh
 
 sudo mkdir -p /Library/LaunchDaemons/
 
-sudo mv com.computernetworkbasics.wifionoff.plist /Library/LaunchDaemons/com.computernetworkbasics.wifionoff.plist
+sudo cp com.computernetworkbasics.wifionoff.plist /Library/LaunchDaemons/com.computernetworkbasics.wifionoff.plist
 
 sudo chown root:wheel /Library/LaunchDaemons/com.computernetworkbasics.wifionoff.plist
