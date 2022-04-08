ffe4f7f7966babd52dcee8f50850d2845b85b98c
Patrick Lewis <pat@lo5t.com>
2022-04-08 09:41:15 -0700

    add install modes to install.sh

--- install.sh
+++ install.sh
@@ -3,19 +3,71 @@
 NETBASICS_PATH="/Library/Scripts/NetBasics"
 LAUNCHDAEMONS_PATH="/Library/LaunchDaemons"
 
-SUDO=''
-if [[ $(id -u) -ne 0 ]]; then
-    SUDO='sudo'
-fi
+PS3='[Please enter your choice]: '
+options=(
+    "install (i): install mode"       # 1
+    "uninstall (ui): uninstall mode" # 2
+    "update (up): update mode"       # 3
+    "quit: Exit from this menu"                        # 4
+    )
 
-$SUDO mkdir -p $NETBASICS_PATH
+function _sudo() {
+    SUDO=''
+    if [[ $(id -u) -ne 0 ]]; then
+        SUDO='sudo'
+    fi
+}
 
-$SUDO cp wireless.sh $NETBASICS_PATH
+function _switch() {
+    _reply="$1"
 
-$SUDO chmod 755 $NETBASICS_PATH/wireless.sh
+    case $_reply in
+        ""|"i"|"install"|"1")
+            _sudo
+            $SUDO mkdir -p $NETBASICS_PATH;
+            $SUDO mkdir -p $LAUNCHDAEMONS_PATH;
+            $SUDO cp wireless.sh $NETBASICS_PATH;
+            $SUDO cp com.computernetworkbasics.wifionoff.plist $LAUNCHDAEMONS_PATH/com.computernetworkbasics.wifionoff.plist;
+            $SUDO chmod 755 $NETBASICS_PATH/wireless.sh;
+            $SUDO chown root:wheel $LAUNCHDAEMONS_PATH/com.computernetworkbasics.wifionoff.plist;
+            ;;
+        ""|"ui"|"uninstall"|"2")
+            _sudo
+            $SUDO rm -Rf $NETBASICS_PATH/wireless.sh;
+            $SUDO rm -Rf $LAUNCHDAEMONS_PATH/com.computernetworkbasics.wifionoff.plist;
+            $SUDO rm -Rf $NETBASICS_PATH;
+            $SUDO rm -Rf $LAUNCHDAEMONS_PATH;
+            ;;
+        ""|"up"|"update"|"3")
+            _sudo
+            $SUDO cp wireless.sh $NETBASICS_PATH;
+            $SUDO cp com.computernetworkbasics.wifionoff.plist $LAUNCHDAEMONS_PATH/com.computernetworkbasics.wifionoff.plist;
+            $SUDO chmod 755 $NETBASICS_PATH/wireless.sh;
+            $SUDO chown root:wheel $LAUNCHDAEMONS_PATH/com.computernetworkbasics.wifionoff.plist;
+            exit
+            ;;
+        ""|"quit"|"4")
+            echo "Goodbye!"
+            exit
+            ;;
+        ""|"--help")
+            echo "Available commands:"
+            printf '%s\n' "${options[@]}"
+            ;;
+        *) echo "invalid option, use --help option for the commands list";;
+    esac
+}
 
-$SUDO mkdir -p $LAUNCHDAEMONS_PATH
+while true
+do
+    # run option directly if specified in argument
+    [ ! -z $1 ] && _switch $@
+    [ ! -z $1 ] && exit 0
 
-$SUDO cp com.computernetworkbasics.wifionoff.plist $LAUNCHDAEMONS_PATH/com.computernetworkbasics.wifionoff.plist
-
-$SUDO chown root:wheel $LAUNCHDAEMONS_PATH/com.computernetworkbasics.wifionoff.plist
+    echo "==== OPTIONS ===="
+    select opt in "${options[@]}"
+    do
+        _switch $REPLY
+        break
+    done
+done
