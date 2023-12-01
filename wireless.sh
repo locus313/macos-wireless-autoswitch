acadbbbf43aba621ad521f083f339fd8a748f457
Patrick Lewis <4015312+locus313@users.noreply.github.com>
2023-11-30 16:43:50 -0800

    remove support for older macos releases (#13)

--- wireless.sh
+++ wireless.sh
@@ -19,8 +19,8 @@ do
   fi
 done
  
-# For OSX 10.7 (#11) Lion and OSX 10.8 (#12) Mountain Lion and 10.9 (#13) Mavericks, Yosemite (#14), El Capitan (#15), Sierra (#16), High Sierra (#17), Mojave (#18), Catalina (#19), Big Sur (#20), Monterey (#21), Ventura (#22)
-if [ "$OSVERSION" == "11" ] || [ "$OSVERSION" == "12" ] || [ "$OSVERSION" == "13" ] || [ "$OSVERSION" == "14" ] || [ "$OSVERSION" == "15" ] || [ "$OSVERSION" == "16" ] || [ "$OSVERSION" == "17" ] || [ "$OSVERSION" == "18" ] || [ "$OSVERSION" == "19" ] || [ "$OSVERSION" == "20" ] || [ "$OSVERSION" == "21" ] || [ "$OSVERSION" == "22" ]; then
+# For Monterey (#21), Ventura (#22)
+if [ "$OSVERSION" == "21" ] || [ "$OSVERSION" == "22" ]; then
   if [ $IPFOUND ]; then
     /usr/sbin/networksetup -setairportpower "$WIFIINTERFACES" off || exit 1
     echo "Turning OFF wireless on card $WIFIINTERFACES."
